d3fe398de58cd260eee548efc2a675ab50c2b27a
Jacqueline Askins <askinsjacqueline@gmail.com>
2021-07-04 09:51:43 -0400

    Neovim: Use enter for confirming compe completions

--- nvim/lua/my_plugins/autopairs.lua
+++ nvim/lua/my_plugins/autopairs.lua
@@ -1,5 +1,2 @@
 require'nvim-autopairs'.setup()
-
-function _G.handle_enter() return require'nvim-autopairs'.autopairs_cr() end
-
-require'my_utils'.map('i', '<CR>', 'v:lua.handle_enter()', {expr = true})
+require('nvim-autopairs.completion.compe').setup({map_cr = true, map_complete = true})
--- nvim/lua/my_plugins/compe.lua
+++ nvim/lua/my_plugins/compe.lua
@@ -6,6 +6,7 @@ opt.completeopt = 'menu,menuone,noselect'
 
 require'compe'.setup {
   enabled = true,
+  documentation = {border = 'single'},
   source = {buffer = true, nvim_lsp = true, nvim_lua = true, vsnip = true},
 }
 
@@ -17,27 +18,28 @@ map('i', '<C-b>', 'compe#scroll({ "delta": -4 })', compe_opts)
 
 local check_back_space = function()
   local col = vim.fn.col('.') - 1
-  if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-    return true
-  else
-    return false
-  end
+  return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
 end
 
 _G.tab_complete = function()
-  if vim.fn.call('vsnip#available', {1}) == 1 then
-    return t '<Plug>(vsnip-expand-or-jump)'
+  if vim.fn.pumvisible() == 1 then
+    return t "<C-n>"
+  elseif vim.fn['vsnip#available'](1) == 1 then
+    return t "<Plug>(vsnip-expand-or-jump)"
   elseif check_back_space() then
-    return t '<Tab>'
+    return t "<Tab>"
   else
-    return vim.fn['compe#confirm']()
+    return vim.fn['compe#complete']()
   end
 end
 _G.s_tab_complete = function()
-  if vim.fn.call('vsnip#jumpable', {-1}) == 1 then
-    return t '<Plug>(vsnip-jump-prev)'
+  if vim.fn.pumvisible() == 1 then
+    return t "<C-p>"
+  elseif vim.fn['vsnip#jumpable'](-1) == 1 then
+    return t "<Plug>(vsnip-jump-prev)"
   else
-    return t '<S-Tab>'
+    -- If <S-Tab> is not working in your terminal, change it to <C-h>
+    return t "<S-Tab>"
   end
 end
 
