a4e7830f8ce2dd9115ad203bdc8cd1e75529a5f2
Jacqueline Askins <askinsjacqueline@gmail.com>
2021-08-22 20:04:18 -0400

    Neovim: Add initial closer functionality

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,6 +2,7 @@ local file_exists = require('utils').file_exists
 
 require('settings')
 require('keymappings')
+require('closer')
 
 if file_exists('~/dotfiles/nvim/lua/custom.lua') then
   require('custom')
---
+++ nvim/lua/closer.lua
@@ -0,0 +1,94 @@
+local fn = vim.fn
+local utils = require('utils')
+local map, t = utils.map, utils.t
+local ts_utils = require('nvim-treesitter.ts_utils')
+
+local standard = {
+  ['{'] = '}',
+  ['('] = ')',
+  ['['] = ']',
+}
+
+local pairs_by_language = {
+  lua = {
+    ['function'] = 'end',
+    ['do'] = 'end',
+    ['then'] = 'end',
+    ['{'] = '}',
+    ['('] = ')',
+    ['['] = ']',
+  },
+}
+
+local function fallback()
+  return t('<CR>')
+end
+
+local function is_in_string(col)
+  local row = fn.line('.') - 1
+
+  local root = ts_utils.get_root_for_position(row, col)
+  if not root then
+    return false
+  end
+
+  local node = root:named_descendant_for_range(row, col, row, col)
+
+  while node ~= nil do
+    if node:type() == 'string' then
+      return true
+    end
+    node = node:parent()
+  end
+
+  return false
+end
+
+function _G.close()
+  local open_pairs = pairs_by_language[vim.bo.filetype] or standard
+
+  local close_pairs = {}
+  for open, close in pairs(open_pairs) do
+    local opens = close_pairs[close] or {}
+    table.insert(opens, open)
+    close_pairs[close] = opens
+  end
+
+  local all_delims = vim.tbl_keys(open_pairs)
+  vim.list_extend(all_delims, vim.tbl_keys(close_pairs))
+  local pattern = '\\V\\(' .. table.concat(all_delims, '\\|') .. '\\)'
+  local stack = {}
+
+  local start = 0
+  local line = string.sub(fn.getline('.'), 1, fn.col('.') - 1)
+
+  while start ~= -1 do
+    local matched, match_start, match_end = unpack(fn.matchstrpos(line, pattern, start))
+    start = match_end
+
+    if is_in_string(match_start) then
+    elseif open_pairs[matched] then
+      table.insert(stack, matched)
+    elseif close_pairs[matched] then
+      local opens = close_pairs[matched]
+      local removed = table.remove(stack, #stack)
+
+      if not vim.tbl_contains(opens, removed) then
+        return fallback()
+      end
+    end
+  end
+
+  if #stack == 0 then
+    return fallback()
+  end
+
+  local ends = {}
+  for _, open in ipairs(stack) do
+    table.insert(ends, 1, open_pairs[open])
+  end
+
+  return t('<CR><CR>' .. table.concat(ends, '') .. '<Esc>==kcc')
+end
+
+map('i', '<CR>', 'v:lua.close()', { expr = true, silent = true })
