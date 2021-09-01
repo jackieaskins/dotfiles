62eb458b500f024a26b88062993475212509e19d
Jacqueline Askins <askinsjacqueline@gmail.com>
2021-08-31 21:32:33 -0400

    Neovim: handle new lines for tags, fix eol check

--- nvim/lua/closer.lua
+++ nvim/lua/closer.lua
@@ -2,6 +2,10 @@ local fn = vim.fn
 local utils = require('utils')
 local map, t = utils.map, utils.t
 
+local function ts_utils()
+  return require('nvim-treesitter.ts_utils')
+end
+
 -- TODO: Allow snippet-like jumping between brackets
 local standard = {
   ['{'] = '}',
@@ -21,8 +25,40 @@ local pairs_by_language = {
   },
 }
 
+local tags = {
+  jsx_closing_element = 'jsx_opening_element',
+  end_tag = 'start_tag',
+}
+
 local function fallback()
   return t('<CR>')
+end
+
+local function check_for_tags(current_node)
+  local cursor = vim.api.nvim_win_get_cursor(0)
+  local start_row, start_col = current_node:start()
+  local open_tag = tags[current_node:type()]
+
+  if open_tag ~= nil then
+    local previous_node = ts_utils().get_previous_node(current_node)
+    if previous_node:type() == open_tag and start_row == cursor[1] - 1 and start_col == cursor[2] then
+      return t('<CR><Esc>O')
+    end
+    return fallback()
+  end
+
+  return nil
+end
+
+local function check_for_fragment(current_node)
+  local start_row, start_col = current_node:start()
+  local end_row = current_node:end_()
+
+  if current_node:type() == 'jsx_fragment' and start_row == end_row and fn.col('.') == start_col + 3 then
+    return t('<CR><Esc>O')
+  end
+
+  return nil
 end
 
 -- TODO: Make this work for multi-line comments
@@ -32,7 +68,7 @@ local function is_comment_or_string(col)
 
   for _, match in ipairs(matches) do
     local node = match.node
-    if node and require('nvim-treesitter.ts_utils').is_in_node_range(node, row, col) then
+    if node and ts_utils().is_in_node_range(node, row, col) then
       return true
     end
   end
@@ -41,6 +77,20 @@ local function is_comment_or_string(col)
 end
 
 function _G.close()
+  local current_node = ts_utils().get_node_at_cursor()
+
+  if current_node ~= nil then
+    local tags_result = check_for_tags(current_node)
+    if tags_result ~= nil then
+      return tags_result
+    end
+
+    local fragment_result = check_for_fragment(current_node)
+    if fragment_result ~= nil then
+      return fragment_result
+    end
+  end
+
   local open_pairs = pairs_by_language[vim.bo.filetype] or standard
 
   local close_pairs = {}
@@ -58,7 +108,7 @@ function _G.close()
   local start = 0
 
   -- TODO: Ideally check for matching brackets instead
-  if fn.col('.') < fn.col('$') - 1 then
+  if fn.col('.') <= fn.col('$') - 1 then
     return fallback()
   end
 
