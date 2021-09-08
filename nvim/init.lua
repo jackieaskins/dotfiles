d8fb5ef2c47082ed228ed699c801bc4748d59fcd
Jacqueline Askins <askinsjacqueline@gmail.com>
2021-09-07 20:26:50 -0400

    Neovim: Remove mpack

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,16 +1,5 @@
 local file_exists = require('utils').file_exists
 
-local lua_version = string.gsub(_VERSION, 'Lua ', '')
-local jit_version = string.gsub(jit.version, 'LuaJIT ', '')
-local packer_install_path = table.concat({
-  '~/.cache/nvim/packer_hererocks/',
-  jit_version,
-  '/lib/lua/',
-  lua_version,
-  '/?.so',
-}, '')
-
-package.cpath = package.cpath .. ';' .. vim.fn.expand(packer_install_path)
 pcall(require, 'impatient')
 
 require('settings')
