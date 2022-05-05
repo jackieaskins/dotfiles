f13657932f7596886442d2e17acc1ed8ad6b2703
Jacqueline Askins <askinsjacqueline@gmail.com>
2022-05-04 23:36:36 -0400

    Hammerspoon: Add stackline

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,9 +1,12 @@
-local BREW_PREFIX = '/opt/homebrew'
-local YABAI_PATH = BREW_PREFIX .. '/bin/yabai'
-local MEH = { 'option', 'shift', 'ctrl' }
-local HYPER = { 'option', 'shift', 'ctrl', 'cmd' }
+----------------------------------------------------------------------
+--                            Constants                             --
+----------------------------------------------------------------------
+BREW_PREFIX = '/opt/homebrew'
+YABAI_PATH = BREW_PREFIX .. '/bin/yabai'
+MEH = { 'option', 'shift', 'ctrl' }
+HYPER = { 'option', 'shift', 'ctrl', 'cmd' }
 
-require('hs.ipc').cliInstall(BREW_PREFIX)
+require('hs.ipc').cliInstall(BREW_PREFIX) -- Enables CLI
 
 ----------------------------------------------------------------------
 --                              Spoons                              --
@@ -17,6 +20,16 @@ spoon.ReloadConfiguration:start()
 spoon.ReloadConfiguration:bindHotkeys({
   reloadConfiguration = { HYPER, 'r' },
 })
+
+----------------------------------------------------------------------
+--                            Stackline                             --
+----------------------------------------------------------------------
+Stackline = require('stackline')
+local conf = hs.fnutils.copy(require('stackline.conf'))
+conf.paths.yabai = YABAI_PATH
+conf.appearance.offset.y = 6
+conf.appearance.offset.x = 6
+Stackline:init(conf)
 
 ----------------------------------------------------------------------
 --                             Watchers                             --
@@ -75,67 +88,6 @@ for key, app in pairs(app_keys) do
   end)
 end
 
-hs.hotkey.bind(MEH, 'r', 'Reloading Yabai', function()
-  os.execute('launchctl kickstart -k "gui/${UID}/homebrew.mxcl.yabai"')
-end)
-
-local function yabai(args, stream_cb, cb)
-  return function()
-    hs.task.new(YABAI_PATH, cb, stream_cb or function()
-      return true
-    end, args):start()
-  end
-end
-
-hs.hotkey.bind(MEH, 'f', yabai({ '-m', 'window', '--toggle', 'zoom-fullscreen' }))
-hs.hotkey.bind(MEH, 'p', yabai({ '-m', 'window', '--toggle', 'zoom-parent' }))
-
-for key, direction in pairs({ h = 'west', j = 'south', k = 'north', l = 'east' }) do
-  hs.hotkey.bind(MEH, key, yabai({ '-m', 'window', '--warp', direction }))
-  hs.hotkey.bind(
-    HYPER,
-    key,
-    yabai({ '-m', 'window', '--focus', direction }, function(_, _, stderr)
-      if stderr ~= '' then
-        yabai({ '-m', 'display', '--focus', direction })()
-      end
-      return true
-    end)
-  )
-end
-
-for key, direction in pairs({ left = 'prev', right = 'next' }) do
-  hs.hotkey.bind(
-    MEH,
-    key,
-    yabai({ '-m', 'window', '--space', direction }, nil, function(_, _, stderr)
-      if stderr == '' then
-        -- Adding fn as a workaround for this issue: https://github.com/Hammerspoon/hammerspoon/issues/1946
-        hs.eventtap.keyStroke({ 'fn', 'ctrl' }, key, 100)
-      end
-    end)
-  )
-  hs.hotkey.bind(
-    HYPER,
-    key,
-    yabai({ '-m', 'window', '--display', direction }, nil, yabai({ '-m', 'display', '--focus', direction }))
-  )
-end
-
-for idx = 1, 9 do
-  local str_idx = tostring(idx)
-  hs.hotkey.bind(
-    MEH,
-    str_idx,
-    yabai({ '-m', 'window', '--space', str_idx }, nil, function()
-      hs.eventtap.keyStroke({ 'ctrl' }, str_idx, 100)
-    end)
-  )
-  hs.hotkey.bind(
-    HYPER,
-    str_idx,
-    yabai({ '-m', 'window', '--display', str_idx }, nil, yabai({ '-m', 'display', '--focus', str_idx }))
-  )
-end
+require('yabai')
 
 hs.alert.show('Loaded HammerSpoon config')
