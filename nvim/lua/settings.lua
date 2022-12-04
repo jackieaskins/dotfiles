9bb31d5a1ed598c7c3cee2da956aeff76f21a0fd
Jacqueline Askins <askinsjacqueline@gmail.com>
2022-12-04 18:57:40 -0500

    Neovim: Try auto-hlsearch

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -47,7 +47,13 @@ vim.opt.updatetime = 100
 
 -- Searching
 vim.opt.inccommand = 'nosplit'
-vim.opt.hlsearch = false
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.path:append({ '**' })
+
+-- Auto hlsearch
+vim.on_key(function(char)
+  if vim.fn.mode() == 'n' then
+    vim.opt.hlsearch = vim.tbl_contains({ '<CR>', 'n', 'N', '*', '#', '?', '/' }, vim.fn.keytrans(char))
+  end
+end, vim.api.nvim_create_namespace('auto_hlsearch'))
