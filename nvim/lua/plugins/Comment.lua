bf7e7d249342650533b948e6dc662015d1425f8f
Jacqueline Askins <askinsjacqueline@gmail.com>
2022-12-26 13:16:39 -0500

    Neovim: Use jsx branch of Comment.nvim

--- nvim/lua/plugins/Comment.lua
+++ nvim/lua/plugins/Comment.lua
@@ -1,36 +1,11 @@
-local M = {
+-- JSX Pull Request: https://github.com/numToStr/Comment.nvim/pull/133
+return {
   'numToStr/Comment.nvim',
-  dependencies = {
-    'JoosepAlviste/nvim-ts-context-commentstring',
-    'nvim-treesitter/nvim-treesitter',
-  },
-}
-
-function M.config()
-  require('Comment').setup({
-    ignore = '^$',
+  branch = 'jsx',
+  config = {
     pre_hook = function(ctx)
-      if vim.bo.filetype == 'typescriptreact' then
-        local U = require('Comment.utils')
-
-        -- Detemine whether to use linewise or blockwise commentstring
-        local type = ctx.ctype == U.ctype.line and '__default' or '__multiline'
-
-        -- Determine the location where to calculate commentstring from
-        local location = nil
-        if ctx.ctype == U.ctype.block then
-          location = require('ts_context_commentstring.utils').get_cursor_location()
-        elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-          location = require('ts_context_commentstring.utils').get_visual_start_location()
-        end
-
-        return require('ts_context_commentstring.internal').calculate_commentstring({
-          key = type,
-          location = location,
-        })
-      end
+      return require('Comment.jsx').calculate(ctx)
     end,
-  })
-end
-
-return M
+  },
+  dependencies = { 'nvim-treesitter/nvim-treesitter' },
+}
