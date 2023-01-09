a7f851ce21c3455e5efdfb26a6764e8ad3bae9b8
Jacqueline Askins <askinsjacqueline@gmail.com>
2023-01-09 18:55:28 -0500

    Karabiner: Make ctrl -> meh mapping conditional, Hammerspoon: Add more apps to window filter

--- hammerspoon/twm/windowFilter.lua
+++ hammerspoon/twm/windowFilter.lua
@@ -4,8 +4,11 @@ local window_filters = mergeTables({
   ['System Preferences'] = false,
   ['System Settings'] = false,
   ['Brave Browser'] = { rejectTitles = 'Picture in Picture' },
+  Finder = { rejectTitles = ' Info$' },
   ['Google Chrome'] = { rejectTitles = 'Picture in Picture' },
-  ['Hammerspoon'] = { rejectTitles = 'Hammerspoon Console' },
+  Hammerspoon = { rejectTitles = 'Hammerspoon Console' },
+  ['Karabiner-Elements'] = false,
+  ['Keychain Access'] = false,
 }, CUSTOM.twm_window_filters or {})
 
 return hs.window.filter
