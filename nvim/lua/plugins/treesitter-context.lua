65889d2da3359c0813ae945ec5d7f0d0fd8ce80a
Jacqueline Askins <askinsjacqueline@gmail.com>
2023-03-27 21:26:05 -0400

    Neovim: Remove unnecessary nvim-treesitter-context opts

--- nvim/lua/plugins/treesitter-context.lua
+++ nvim/lua/plugins/treesitter-context.lua
@@ -1,27 +1,5 @@
 return {
   'nvim-treesitter/nvim-treesitter-context',
   dependencies = { 'nvim-treesitter/nvim-treesitter' },
-  opts = {
-    patterns = {
-      default = {
-        'class',
-        'function',
-        'method',
-        'for',
-        'while',
-        'if',
-        'elseif',
-        'else',
-        'switch',
-        'case',
-        'interface',
-        'struct',
-        'enum',
-      },
-      javascript = { 'arrow_function' },
-      javascriptreact = { 'arrow_function' },
-      typescript = { 'arrow_function', 'export_statement' },
-      typescriptreact = { 'arrow_function', 'export_statement' },
-    },
-  },
+  config = true,
 }
