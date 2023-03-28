993273454fa6cecb7b00ff749dd66164a49e47ca
Jacqueline Askins <askinsjacqueline@gmail.com>
2023-03-27 21:28:59 -0400

    Zsh: Add function to copy file to multiple directories

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,4 +1,9 @@
-# Functions for general use
+# Make directory and cd into it
 function mk() {
   mkdir -p $1 && cd $1
+}
+
+# Copy single file to multiple directories
+function cp2m() {
+  xargs -n 1 cp -v $1 <<< "${@:2}"
 }
