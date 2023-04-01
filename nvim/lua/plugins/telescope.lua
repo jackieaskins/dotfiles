d39326f55c701e7762090a76ae65b5c715ecb80d
Jacqueline Askins <askinsjacqueline@gmail.com>
2023-04-01 14:44:17 -0400

    Neovim: Telescope: add lazy plugin, update ignore patterns, add git keymaps

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -5,6 +5,7 @@ local M = {
     { 'benfowler/telescope-luasnip.nvim' },
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     { 'danielvolchek/tailiscope.nvim' },
+    { 'tsakirist/telescope-lazy.nvim' },
   },
   cmd = 'Telescope',
   keys = {
@@ -16,12 +17,16 @@ local M = {
     { '<leader>rg', ':Telescope grep_string search=', { silent = false } },
     { '<leader>/', '<cmd>Telescope live_grep only_sort_text=true<CR>' },
     { '<leader>fw', '<cmd>Telescope grep_string<CR>' },
-    { '<leader>gs', '<cmd>Telescope git_status<CR>' },
     { '<leader>bu', '<cmd>Telescope buffers sort_mru=true<CR>' },
     { '<leader>of', '<cmd>Telescope oldfiles cwd_only=true sort_lastused=true include_current_session=true<CR>' },
     { '<leader>wd', '<cmd>Telescope diagnostics<CR>' },
     { '<leader>z=', '<cmd>Telescope spell_suggest<CR>' },
     { '<leader>sn', '<cmd>Telescope luasnip<CR>' },
+
+    -- Git commands
+    { '<leader>gs', '<cmd>Telescope git_status<CR>' },
+    { '<leader>gl', '<cmd>Telescope git_commits<CR>' },
+    { '<leader>gL', '<cmd>Telescope git_bcommits<CR>' },
   },
 }
 
@@ -31,12 +36,14 @@ function M.config()
   local action_layout = require('telescope.actions.layout')
   local action_set = require('telescope.actions.set')
 
+  local default_file_ignore_patterns = {
+    '.git/*',
+    'dist/*',
+  }
+
   telescope.setup({
     defaults = {
-      file_ignore_patterns = {
-        'node_modules/*',
-        '.git/*',
-      },
+      file_ignore_patterns = default_file_ignore_patterns,
       layout_strategy = 'vertical',
       mappings = {
         i = {
@@ -67,9 +74,16 @@ function M.config()
         case_mode = 'smart_case',
       },
     },
+    pickers = {
+      find_files = {
+        file_ignore_patterns = vim.tbl_extend('keep', default_file_ignore_patterns, { 'node_modules/*' }),
+        live_grep = vim.tbl_extend('keep', default_file_ignore_patterns, { 'package_lock.json' }),
+      },
+    },
   })
 
   telescope.load_extension('fzf')
+  telescope.load_extension('lazy')
   telescope.load_extension('luasnip')
   telescope.load_extension('notify')
 end
