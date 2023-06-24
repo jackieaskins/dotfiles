1578cd3342e2fe084a954f6d2c55cb9c86a907d0
Jacqueline Askins <askinsjacqueline@gmail.com>
2023-06-24 13:33:04 -0500

    Neovim: Move checkThirdParty to .luarc.json, remove globals

--- nvim/lua/lsp/servers/lua_ls.lua
+++ nvim/lua/lsp/servers/lua_ls.lua
@@ -3,9 +3,7 @@ return {
     config.settings = {
       Lua = {
         completion = { keywordSnippet = 'Replace', callSnippet = 'Replace' },
-        diagnostics = { globals = { 'hs', 'vim' } },
         hint = { arrayIndex = 'Disable', enable = true },
-        workspace = { checkThirdParty = false },
       },
     }
 
