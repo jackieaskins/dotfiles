9a9de864200dcfd54f47d9ced188c95cdcf667a3
Jacqueline Askins <askinsjacqueline@gmail.com>
2023-09-27 20:43:17 -0400

    Neovim: Scope autolist keymaps to buffer

--- nvim/lua/plugins/autolist.lua
+++ nvim/lua/plugins/autolist.lua
@@ -1,28 +1,41 @@
+local ft = { 'markdown', 'norg', 'plaintex', 'tex', 'text' }
+
 return {
   'gaoDean/autolist.nvim',
-  ft = { 'markdown', 'norg', 'plaintex', 'tex', 'text' },
-  config = function()
-    local map = require('utils').map
-    local autolist = require('autolist')
+  config = true,
+  ft = ft,
+  init = function()
+    local utils = require('utils')
+    utils.augroup('autolist', {
+      {
+        'FileType',
+        pattern = ft,
+        callback = function(args)
+          local bsk = utils.buffer_map(args.buf)
 
-    autolist.setup()
+          bsk('i', '<tab>', '<cmd>AutolistTab<CR>')
+          bsk('i', '<s-tab>', '<cmd>AutolistShiftTab<CR>')
+          bsk('i', '<CR>', '<CR><cmd>AutolistNewBullet<CR>')
+          bsk('n', 'o', 'o<cmd>AutolistNewBullet<CR>')
+          bsk('n', 'O', 'O<cmd>AutolistNewBulletBefore<CR>')
+          bsk('n', '<CR>', '<cmd>AutolistToggleCheckbox<CR><CR>')
+          bsk('n', '<leader>cr', '<cmd>AutolistRecalculate<CR>')
 
-    map('i', '<tab>', '<cmd>AutolistTab<CR>')
-    map('i', '<s-tab>', '<cmd>AutolistShiftTab<CR>')
-    map('i', '<CR>', '<CR><cmd>AutolistNewBullet<CR>')
-    map('n', 'o', 'o<cmd>AutolistNewBullet<CR>')
-    map('n', 'O', 'O<cmd>AutolistNewBulletBefore<CR>')
-    map('n', '<CR>', '<cmd>AutolistToggleCheckbox<CR><CR>')
-    map('n', '<C-r>', '<cmd>AutolistRecalculate<CR>')
+          -- cycle list types with dot-repeat
+          bsk('n', '<leader>cn', function()
+            require('autolist').cycle_next_dr()
+          end, { expr = true })
+          bsk('n', '<leader>cp', function()
+            require('autolist').cycle_prev_dr()
+          end, { expr = true })
 
-    -- cycle list types with dot-repeat
-    map('n', '<leader>cn', autolist.cycle_next_dr, { expr = true })
-    map('n', '<leader>cp', autolist.cycle_prev_dr, { expr = true })
-
-    -- functions to recalculate list on edit
-    map('n', '>>', '>><cmd>AutolistRecalculate<CR>')
-    map('n', '<<', '<<<cmd>AutolistRecalculate<CR>')
-    map('n', 'dd', 'dd<cmd>AutolistRecalculate<CR>')
-    map('v', 'd', 'd<cmd>AutolistRecalculate<CR>')
+          -- functions to recalculate list on edit
+          bsk('n', '>>', '>><cmd>AutolistRecalculate<CR>')
+          bsk('n', '<<', '<<<cmd>AutolistRecalculate<CR>')
+          bsk('n', 'dd', 'dd<cmd>AutolistRecalculate<CR>')
+          bsk('v', 'd', 'd<cmd>AutolistRecalculate<CR>')
+        end,
+      },
+    })
   end,
 }
