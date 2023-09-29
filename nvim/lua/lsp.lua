c8f8920439acc9d447e3a1953f7723ac4c7f5fd2
Jacqueline Askins <askinsjacqueline@gmail.com>
2023-09-28 20:45:51 -0400

    Neovim: Make lsp hover silent

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,5 +1,6 @@
 vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
   border = vim.g.border_style,
+  silent = true,
 })
 
 vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, {
