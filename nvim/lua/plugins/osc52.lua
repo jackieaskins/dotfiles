901cfdbbd985ec9349590f89154045b6da97f5b3
Jacqueline Askins <askinsjacqueline@gmail.com>
2023-10-25 20:01:51 -0400

    Neovim, Tmux: Setup nvim-osc52 for copying to clipboard

---
+++ nvim/lua/plugins/osc52.lua
@@ -0,0 +1,26 @@
+return {
+  'ojroques/nvim-osc52',
+  config = function()
+    require('osc52').setup({
+      tmux_passthrough = true,
+    })
+
+    local function copy(lines, _)
+      require('osc52').copy(table.concat(lines, '\n'))
+    end
+
+    local function paste()
+      return {
+        ---@diagnostic disable-next-line: param-type-mismatch
+        vim.fn.split(vim.fn.getreg(''), '\n'),
+        vim.fn.getregtype(''),
+      }
+    end
+
+    vim.g.clipboard = {
+      name = 'osc52',
+      copy = { ['+'] = copy, ['*'] = copy },
+      paste = { ['+'] = paste, ['*'] = paste },
+    }
+  end,
+}
