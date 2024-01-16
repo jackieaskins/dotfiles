b554a8191420e33e9d7995d011cede0bbc7c10bf
Jacqueline Askins <askinsjacqueline@gmail.com>
2024-01-15 21:12:57 -0500

    Neovim: Fix newlines in tags with ultimate-autopair

--- nvim/lua/plugins/ultimate-autopair.lua
+++ nvim/lua/plugins/ultimate-autopair.lua
@@ -3,6 +3,8 @@ return {
   enabled = vim.g.use_ultimate_pairs,
   event = { 'InsertEnter', 'CmdlineEnter' },
   opts = {
+    -- Mapping to get newline to work properly for html tags (doesn't attempt to do any autocompletion)
+    { '>', '<', newline = true, disable_start = true },
     close = { enable = true },
     cmap = false,
     cr = { enable = true, autoclose = true },
