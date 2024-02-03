35c95672841554ef14b952f73685b241b40776ab
Jacqueline Askins <askinsjacqueline@gmail.com>
2024-02-02 21:21:22 -0500

    Neovim: Update kitty and tmux filetype detection

--- nvim/filetype.lua
+++ nvim/filetype.lua
@@ -1,17 +1,25 @@
 vim.filetype.add({
+  extension = {
+    conf = function(path)
+      if path:match('tmux') then
+        return 'tmux'
+      end
+
+      if path:match('kitty') then
+        return 'kitty'
+      end
+
+      return 'conf'
+    end,
+  },
   filename = {
     Brewfile = 'ruby',
-    ['tmux.conf'] = 'tmux',
     ['coc-settings.json'] = 'jsonc',
     ['.eslintrc.json'] = 'jsonc',
-    ['kitty.conf'] = 'kitty',
   },
   pattern = {
     ['jdt://.*'] = 'java',
-    ['tmux/.*%.conf'] = 'tmux',
     ['tsconfig.*%.json'] = 'jsonc',
     ['%.env.*'] = 'sh',
-    ['kitty/.*%.conf'] = 'kitty',
-    ['kitty/.*%.session'] = 'kitty-session',
   },
 })
