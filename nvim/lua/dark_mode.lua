59b7dd1cc4f87c227c875c2ddb102a861f889177
Jacqueline Askins <askinsjacqueline@gmail.com>
2024-02-05 19:18:49 -0500

    Neovim: Set tint to lighten in light mode

--- nvim/lua/dark_mode.lua
+++ nvim/lua/dark_mode.lua
@@ -1,10 +1,14 @@
 local watcher = vim.uv.new_fs_event()
 local theme_file = vim.fn.expand('~/dotfiles/theme')
 
-local function set_background()
+local function set_background(reload_tint)
   local file = io.open(theme_file, 'r')
   local background = file and file:read() or 'dark'
   vim.opt.background = background
+
+  if reload_tint then
+    vim.cmd('Lazy reload tint.nvim')
+  end
 end
 
 local function watch_for_theme_change()
@@ -16,12 +20,12 @@ local function watch_for_theme_change()
     theme_file,
     {},
     vim.schedule_wrap(function()
-      set_background()
+      set_background(true)
       watcher:stop()
       watch_for_theme_change()
     end)
   )
 end
 
-set_background()
+set_background(false)
 watch_for_theme_change()
--- nvim/lua/plugins/tint.lua
+++ nvim/lua/plugins/tint.lua
@@ -1,23 +1,25 @@
 return {
   'levouh/tint.nvim',
-  opts = {
-    tint = -45,
-    saturation = 0.6,
-    highlight_ignore_patterns = {
-      '@comment',
-      '@ibl.indent.char.1',
-      'Comment',
-      'DapUIUnavailable',
-      'EndOfBuffer',
-      '^LineNr',
-      'Whitespace',
-      'WinbarNC',
-      'WinSeparator',
-    },
-    window_ignore_function = function(winid)
-      local bufid = vim.api.nvim_win_get_buf(winid)
-      local filetype = vim.api.nvim_get_option_value('filetype', { buf = bufid })
-      return vim.startswith(filetype, 'Telescope')
-    end,
-  },
+  opts = function()
+    return {
+      tint = vim.o.background == 'dark' and -45 or 60,
+      saturation = 0.6,
+      highlight_ignore_patterns = {
+        '@comment',
+        '@ibl.indent.char.1',
+        'Comment',
+        'DapUIUnavailable',
+        'EndOfBuffer',
+        '^LineNr',
+        'Whitespace',
+        'WinbarNC',
+        'WinSeparator',
+      },
+      window_ignore_function = function(winid)
+        local bufid = vim.api.nvim_win_get_buf(winid)
+        local filetype = vim.api.nvim_get_option_value('filetype', { buf = bufid })
+        return vim.startswith(filetype, 'Telescope')
+      end,
+    }
+  end,
 }
