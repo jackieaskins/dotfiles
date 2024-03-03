f7036a4cff9d1d9eb7f3c48f822dda243ee221b0
Jacqueline Askins <askinsjacqueline@gmail.com>
2024-03-02 23:23:37 -0500

    Neovim: Remove old invalid svelte textsubjects

--- nvim/after/queries/svelte/textsubjects-smart.scm
+++ nvim/after/queries/svelte/textsubjects-smart.scm
@@ -7,19 +7,15 @@
 
   ; Logic blocks
   (await_statement)
-  (then_statement)
-  (catch_statement)
+  (then_block)
+  (catch_block)
 
   (each_statement)
 
   (if_statement)
-  (else_if_statement)
-  (else_statement)
+  (else_if_block)
+  (else_block)
 
   (key_statement)
-
-  ; Special tags
-  (const_expr)
-  (html_expr)
 ] @_start @_end)
 (#make-range! "range" @_start @_end))
