f6eabafdda2980886e198668ed195556c304039e
Jacqueline Askins <askinsjacqueline@gmail.com>
2024-03-22 23:39:19 -0400

    Neovim: Add garbage-day plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -66,4 +66,5 @@ return {
   { 'tpope/vim-surround' },
   { 'tzachar/highlight-undo.nvim', config = true, keys = { '<C-r>', 'u' } },
   { 'wintermute-cell/gitignore.nvim', cmd = 'Gitignore' },
+  { 'zeioth/garbage-day.nvim', dependencies = 'neovim/nvim-lspconfig', event = 'VeryLazy', config = true },
 }
