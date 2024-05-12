3b4d021c233e2d7914e1d3329aa5e4a7fcb5240d
Jacqueline Askins <askinsjacqueline@gmail.com>
2024-05-12 19:24:34 -0400

    Neovim: Remove opacity from fidget window

--- nvim/lua/plugins/fidget.lua
+++ nvim/lua/plugins/fidget.lua
@@ -1,7 +1,12 @@
 return {
   'j-hui/fidget.nvim',
   opts = {
-    notification = { window = { border = vim.g.border_style } },
+    notification = {
+      window = {
+        border = vim.g.border_style,
+        winblend = 0,
+      },
+    },
     integration = {
       ['nvim-tree'] = { enable = false },
       ['xcodebuild-nvim'] = { enable = false },
