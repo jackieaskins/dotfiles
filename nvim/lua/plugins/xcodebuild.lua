d9f916ac84bf2ec0377f5c0ae5fbc5b10bce2d12
Jacqueline Askins <askinsjacqueline@gmail.com>
2024-05-13 21:47:06 -0400

    Neovim: Use fidget for xcodebuild.nvim notifications

--- nvim/lua/plugins/xcodebuild.lua
+++ nvim/lua/plugins/xcodebuild.lua
@@ -1,3 +1,5 @@
+local progress_handle
+
 return {
   'wojciech-kulik/xcodebuild.nvim',
   enabled = require('lsp.utils').is_server_supported('sourcekit'),
@@ -7,7 +9,40 @@ return {
     'nvim-tree/nvim-tree.lua',
     'nvim-treesitter/nvim-treesitter',
   },
-  config = true,
+  opts = {
+    show_build_progress_bar = false,
+    logs = {
+      notify = function(message, severity)
+        local fidget = require('fidget')
+
+        if progress_handle then
+          progress_handle.message = message
+          if not message:find('Loading') then
+            progress_handle:finish()
+            progress_handle = nil
+            if vim.trim(message) ~= '' then
+              fidget.notify(message, severity)
+            end
+          end
+        else
+          fidget.notify(message, severity)
+        end
+      end,
+      notify_progress = function(message)
+        local progress = require('fidget.progress')
+
+        if progress_handle then
+          progress_handle.title = ''
+          progress_handle.message = message
+        else
+          progress_handle = progress.handle.create({
+            message = message,
+            lsp_client = { name = 'xcodebuild.nvim' },
+          })
+        end
+      end,
+    },
+  },
   init = function()
     require('installer').register('xcode', {
       ['xcode-build-server'] = { 'brew', 'xcode-build-server' },
