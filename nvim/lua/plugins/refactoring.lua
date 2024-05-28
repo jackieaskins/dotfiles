e069c9632d53709beee3e60e864a5c5b44bd9a22
Jacqueline Askins <askinsjacqueline@gmail.com>
2024-05-27 21:17:42 -0400

    Neovim: Add refactoring.nvim plugin

---
+++ nvim/lua/plugins/refactoring.lua
@@ -0,0 +1,25 @@
+local function refactor_key(mode, key, refactor_type)
+  return {
+    key,
+    function()
+      require('refactoring').refactor(refactor_type)
+    end,
+    mode = mode,
+    desc = refactor_type,
+  }
+end
+
+return {
+  'ThePrimeagen/refactoring.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim', 'nvim-treesitter/nvim-treesitter' },
+  config = true,
+  keys = {
+    refactor_key('x', '<leader>re', 'Extract Function'),
+    refactor_key('x', '<leader>rf', 'Extract Function To File'),
+    refactor_key('x', '<leader>rv', 'Extract Variable'),
+    refactor_key('n', '<leader>rI', 'Inline Function'),
+    refactor_key({ 'n', 'x' }, '<leader>ri', 'Inline Variable'),
+    refactor_key('n', '<leader>rb', 'Extract Block'),
+    refactor_key('n', '<leader>rbf', 'Extract Block To File'),
+  },
+}
