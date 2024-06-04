9ebd95f3718ad3976d5a97668b31d5b39fee689e
Jacqueline Askins <askinsjacqueline@gmail.com>
2024-06-03 22:05:24 -0400

    Neovim: Remove git-conflict mappings now that default are correct

--- nvim/lua/plugins/git-conflict.lua
+++ nvim/lua/plugins/git-conflict.lua
@@ -4,14 +4,5 @@ return {
   keys = {
     { '<leader>gc', vim.cmd.GitConflictListQf, 'GitConflictListQf', desc = 'GitConflictListQf' },
   },
-  opts = {
-    default_mappings = {
-      ours = 'co',
-      theirs = 'ct',
-      none = 'c0',
-      both = 'cb',
-      next = ']x',
-      prev = '[x',
-    },
-  },
+  config = true,
 }
