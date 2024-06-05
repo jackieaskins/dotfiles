3eae10d3afa9887abd95e473c3a41f6d9fb36a80
Jacqueline Askins <askinsjacqueline@gmail.com>
2024-06-04 21:04:15 -0400

    Neovim: Properly type augroup util function

--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -51,9 +51,28 @@ function M.highlight(name, highlight_map)
   vim.api.nvim_set_hl(0, name, highlight_map)
 end
 
+---@class AutoCmdCallbackArgs
+---@field id number
+---@field event string
+---@field group number | nil
+---@field match string
+---@field buf number
+---@field file string
+---@field data any
+
+---@class AutoCmd
+---@field [1] string | string[]
+---@field pattern? string | string[]
+---@field buffer? number
+---@field desc? string
+---@field callback? fun(args: AutoCmdCallbackArgs) | string
+---@field command? string
+---@field once? boolean
+---@field nested? boolean
+
 ---Create augroup
 ---@param group_name string
----@param autocmds any
+---@param autocmds AutoCmd[]
 function M.augroup(group_name, autocmds)
   local group = vim.api.nvim_create_augroup('my_' .. group_name, {})
 
