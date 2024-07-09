70fb666caa6d8660b0e2581781644eeb20683f3a
Jacqueline Askins <askinsjacqueline@gmail.com>
2024-07-08 23:20:34 -0400

    fix(neovim): try to fix current line diagnostic virtual text
    - Removed DiagnosticChanged event from autocmd since this seems to cause
      a lot of problems
    - Do not use in lazy windows since it has virtual text enabled on every
      line instead

--- nvim/lua/diagnostic.lua
+++ nvim/lua/diagnostic.lua
@@ -13,12 +13,17 @@ vim.diagnostic.config({
 })
 
 local ns = vim.api.nvim_create_namespace('CurlineDiag')
-local opts = { spacing = 1 }
+local ignored_fts = { 'lazy' }
 require('utils').augroup('curline_diagnostic_vt', {
   {
-    { 'CursorHold', 'DiagnosticChanged', 'ModeChanged' },
+    { 'CursorHold', 'ModeChanged' },
     callback = function(args)
       local bufnr = args.buf
+      local ft = vim.api.nvim_get_option_value('filetype', { buf = bufnr })
+
+      if vim.list_contains(ignored_fts, ft) then
+        return
+      end
 
       pcall(vim.api.nvim_buf_clear_namespace, bufnr, ns, 0, -1)
 
@@ -30,7 +35,7 @@ require('utils').augroup('curline_diagnostic_vt', {
       local diagnostics = vim.diagnostic.get(bufnr, { lnum = lnum })
 
       -- This will eventually break when neovim stops exporting _get_virt_text_chunks
-      local virt_text = vim.diagnostic._get_virt_text_chunks(diagnostics, opts)
+      local virt_text = vim.diagnostic._get_virt_text_chunks(diagnostics, { spacing = 1 })
       if virt_text then
         pcall(vim.api.nvim_buf_set_extmark, bufnr, ns, lnum, 0, {
           hl_mode = 'combine',
