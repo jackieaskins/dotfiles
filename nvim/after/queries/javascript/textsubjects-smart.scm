40ddd25e5370fbe53a9f34406cd77d58692659e7
Jacqueline Askins <askinsjacqueline@gmail.com>
2024-08-28 20:06:40 -0400

    feat(neovim): add additional ecma smart textsubjects

--- nvim/after/queries/javascript/textsubjects-smart.scm
+++ nvim/after/queries/javascript/textsubjects-smart.scm
@@ -1,4 +1,13 @@
 ; extends
 
-((export_statement) @_start @_end
+(((import_specifier) @_start @_end
+  .
+  ","? @_end)
+  (#make-range! "range" @_start @_end))
+
+([
+  (export_statement)
+  (import_statement)
+  (import_clause)
+] @_start @_end
   (#make-range! "range" @_start @_end))
--- nvim/after/queries/tsx/textsubjects-smart.scm
+++ nvim/after/queries/tsx/textsubjects-smart.scm
@@ -1,4 +1,24 @@
 ; extends
 
-((export_statement) @_start @_end
+(((property_signature) @_start @_end
+  .
+  [
+    ","
+    ";"
+  ]? @_end)
+  (#make-range! "range" @_start @_end))
+
+(([
+  (import_specifier)
+  (enum_assignment)
+] @_start @_end
+  .
+  ","? @_end)
+  (#make-range! "range" @_start @_end))
+
+([
+  (export_statement)
+  (import_statement)
+  (import_clause)
+] @_start @_end
   (#make-range! "range" @_start @_end))
