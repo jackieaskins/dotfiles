ba3752bc4845980a123ad63e8baa6531da00b9c1
Jacqueline Askins <askinsjacqueline@gmail.com>
2024-09-07 13:47:05 -0400

    feat(neovim): use single keymap for refactoring.nvim select

--- nvim/lua/plugins/editing/refactoring.lua
+++ nvim/lua/plugins/editing/refactoring.lua
@@ -1,26 +1,16 @@
-local function refactor_key(mode, key, refactor_type)
-  return {
-    key,
-    function()
-      require('refactoring').refactor(refactor_type)
-    end,
-    mode = mode,
-    desc = refactor_type,
-  }
-end
-
 ---@type LazySpec
 return {
   'ThePrimeagen/refactoring.nvim',
   dependencies = { 'nvim-lua/plenary.nvim', 'nvim-treesitter/nvim-treesitter' },
   config = true,
   keys = {
-    refactor_key('x', '<leader>re', 'Extract Function'),
-    refactor_key('x', '<leader>rf', 'Extract Function To File'),
-    refactor_key('x', '<leader>rv', 'Extract Variable'),
-    refactor_key('n', '<leader>rI', 'Inline Function'),
-    refactor_key({ 'n', 'x' }, '<leader>ri', 'Inline Variable'),
-    refactor_key('n', '<leader>rb', 'Extract Block'),
-    refactor_key('n', '<leader>rB', 'Extract Block To File'),
+    {
+      '<leader>rf',
+      function()
+        require('refactoring').select_refactor({})
+      end,
+      mode = { 'x', 'n' },
+      desc = 'Show refactoring select',
+    },
   },
 }
