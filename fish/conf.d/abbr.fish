85ee8d66dc2df12b8f2e5baa654fbb1a5e2c070d
Jacqueline Askins <askinsjacqueline@gmail.com>
2024-09-21 11:23:35 -0400

    fix(fish): fix dotfiles abbr

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -1,7 +1,7 @@
 abbr -a dotdot --regex '^\.\.+$' --function multicd
 abbr -a - "cd -"
 
-abbr -a dotfiles "cd dotfiles"
+abbr -a dotfiles "cd ~/dotfiles"
 abbr -a mux "tmuxinator start"
 abbr -a srcfsh "source ~/.config/fish/config.fish"
 abbr -a plugs "cd ~/.local/share/nvim/lazy/"
