ca1f8e669a1da994b473da85f67c13f920e5b0a2
Jacqueline Askins <askinsjacqueline@gmail.com>
2024-10-07 21:40:06 -0400

    chore: remove git_autofetch function, it's not doing anything

--- fish/functions/git_autofetch.fish
+++
@@ -1,6 +0,0 @@
-function git_autofetch --on-event fish_prompt --description "automatically git fetch"
-    set --local hasGit (find ./ -maxdepth 1 -type d -name .git -print)
-    if test "$hasGit" = "./.git"
-        git fetch
-    end
-end
