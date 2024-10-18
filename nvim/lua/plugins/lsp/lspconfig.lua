670d2bf8d984a8dbbdae7885560c5033e3b22da1
Jacqueline Askins <askinsjacqueline@gmail.com>
2024-10-18 17:49:56 -0400

    fix(neovim): remove nvim-cmp dependency so magazine is used instead

--- nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/lua/plugins/lsp/lspconfig.lua
@@ -1,7 +1,6 @@
 ---@type LazySpec
 return {
   'neovim/nvim-lspconfig',
-  dependencies = { 'hrsh7th/nvim-cmp' },
   event = 'VeryLazy',
   config = function()
     local servers = require('lsp.servers')
