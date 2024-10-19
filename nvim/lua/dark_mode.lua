fa90f2a0ae1ae17d3c0e80d65df02ac01275d599
Jacqueline Askins <askinsjacqueline@gmail.com>
2024-10-19 15:54:36 -0400

    fix(neovim): ensure background is set before catppuccin starts

--- nvim/lua/dark_mode.lua
+++ nvim/lua/dark_mode.lua
@@ -1,14 +1,13 @@
 local watcher = vim.uv.new_fs_event()
 local appearance_file = vim.fn.expand('~/.appearance')
 
+local function set_background()
+  local file = io.open(appearance_file, 'r')
+  local background = file and file:read() or 'dark'
+  vim.o.background = background
+end
+
+set_background()
 if watcher then
-  watcher:start(
-    appearance_file,
-    {},
-    vim.schedule_wrap(function()
-      local file = io.open(appearance_file, 'r')
-      local background = file and file:read() or 'dark'
-      vim.o.background = background
-    end)
-  )
+  watcher:start(appearance_file, {}, vim.schedule_wrap(set_background))
 end
