01b023b7ab125e94b83c9038945482efa9d0ef0e
Jacqueline Askins <askinsjacqueline@gmail.com>
2024-12-12 09:27:20 -0500

    fix(neovim): fix markdown-preview build command

--- nvim/lua/plugins/misc/markdown-preview.lua
+++ nvim/lua/plugins/misc/markdown-preview.lua
@@ -1,9 +1,7 @@
 ---@type LazySpec
 return {
   'iamcco/markdown-preview.nvim',
-  build = function()
-    vim.fn['mkdp#util#install']()
-  end,
+  build = ':call mkdp#util#install()',
   ft = 'markdown',
   keys = {
     { '<leader>mp', vim.cmd.MarkdownPreview, desc = 'MarkdownPreview' },
