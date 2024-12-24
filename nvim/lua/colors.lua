54325386820505421e1b6ca68c61bb24cd5803d3
Jacqueline Askins <askinsjacqueline@gmail.com>
2024-12-24 18:12:48 -0500

    chore(neovim): add type to get_colors function

--- nvim/lua/colors.lua
+++ nvim/lua/colors.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+---Get Catppuccin color palette
+---@return table<string, CtpColors>
 function M.get_colors()
   local ok, palettes = pcall(require, 'catppuccin.palettes')
   return ok and palettes.get_palette() or {}
