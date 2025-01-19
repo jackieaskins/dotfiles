2b57e5596df697a0ff670d8eacbb72ea9c88b7b6
Jacqueline Askins <askinsjacqueline@gmail.com>
2025-01-19 07:57:05 -0500

    fix(neovim): notify svelte ls when a js/ts file changes

--- nvim/lua/lsp/servers/svelte.lua
+++ nvim/lua/lsp/servers/svelte.lua
@@ -16,6 +16,16 @@ return {
 
     config.on_attach = function(client, bufnr)
       require('lsp.utils').setup_auto_close_tag(client, bufnr, 'html/tag')
+
+      require('utils').augroup('svelte_tsjschanges', {
+        {
+          { 'BufWritePost' },
+          pattern = { '*.js', '*.ts' },
+          callback = function(args)
+            client:notify('$/onDidChangeTsOrJsFile', { uri = args.match })
+          end,
+        },
+      })
     end
 
     return config
