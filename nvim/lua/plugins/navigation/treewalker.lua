9ec4644016a2d5d9a139f7e7a6d04799ece16c8f
Jacqueline Askins <askinsjacqueline@gmail.com>
2025-02-20 19:11:53 -0500

    feat(neovim.treewalker): add swap mappings

--- nvim/lua/plugins/navigation/treewalker.lua
+++ nvim/lua/plugins/navigation/treewalker.lua
@@ -9,5 +9,10 @@ return {
     { '<M-j>', '<cmd>Treewalker Down<CR>' },
     { '<M-k>', '<cmd>Treewalker Up<CR>' },
     { '<M-l>', '<cmd>Treewalker Right<CR>' },
+
+    { '<M-S-h>', '<cmd>Treewalker SwapLeft<CR>' },
+    { '<M-S-j>', '<cmd>Treewalker SwapDown<CR>' },
+    { '<M-S-k>', '<cmd>Treewalker SwapUp<CR>' },
+    { '<M-S-l>', '<cmd>Treewalker SwapRight<CR>' },
   },
 }
