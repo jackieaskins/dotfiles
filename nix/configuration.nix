18f80983a2e3c2290f4b1f1247940b44666eb116
Jacqueline Askins <askinsjacqueline@gmail.com>
2025-03-08 09:14:57 -0500

    feat(nix): add aerospace

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -1,14 +1,16 @@
 { pkgs, ... }:
+let
+  aerospaceWindowGap = 15;
+  meh = "ctrl-alt-shift";
+  hyper = "${meh}-cmd";
+in
 {
   system.stateVersion = 5;
 
-  nixpkgs.config.allowUnfree = true;
-
-  nixpkgs.hostPlatform = "aarch64-darwin";
-
   nix.settings.experimental-features = "nix-command flakes";
 
-  programs.zsh.enable = true;
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.hostPlatform = "aarch64-darwin";
 
   environment.systemPackages = [
     pkgs.iina
@@ -16,19 +18,10 @@
     pkgs.qmk
   ];
 
-  services.jankyborders = {
-    enable = true;
-    active_color = "0xFFA6DA95";
-    width = 8.0;
-    hidpi = true;
-    inactive_color = "";
-  };
-
   homebrew = {
     enable = true;
     casks = [
       "alcove"
-      "firefox"
       "ghostty"
       "hammerspoon"
       "jordanbaird-ice"
@@ -42,7 +35,172 @@
     onActivation.upgrade = true;
   };
 
+  programs.zsh.enable = true;
+
   security.pam.services.sudo_local.touchIdAuth = true;
+
+  services.aerospace = {
+    enable = true;
+    settings = {
+      accordion-padding = aerospaceWindowGap;
+      automatically-unhide-macos-hidden-apps = true;
+      on-window-detected = [
+        {
+          "if" = {
+            window-title-regex-substring = "Picture.*in.*Picture";
+          };
+          run = "layout floating";
+        }
+        {
+          "if" = {
+            window-title-regex-substring = "Hammerspoon Console";
+          };
+          run = "layout floating";
+        }
+        {
+          "if" = {
+            app-id = "com.apple.AppStore";
+          };
+          run = "layout floating";
+        }
+        {
+          "if" = {
+            app-id = "com.apple.FaceTime";
+          };
+          run = "layout floating";
+        }
+        {
+          "if" = {
+            app-id = "com.apple.Notes";
+          };
+          run = "layout floating";
+        }
+        {
+          "if" = {
+            app-id = "com.mitchellh.ghostty";
+          };
+          run = "move-node-to-workspace 1";
+        }
+        {
+          "if" = {
+            app-id = "com.hnc.Discord";
+          };
+          run = "move-node-to-workspace 2";
+        }
+        {
+          "if" = {
+            app-id = "com.apple.MobileSMS";
+          };
+          run = "move-node-to-workspace 2";
+        }
+      ];
+      gaps = {
+        inner = {
+          horizontal = aerospaceWindowGap;
+          vertical = aerospaceWindowGap;
+        };
+        outer = {
+          left = aerospaceWindowGap;
+          bottom = aerospaceWindowGap;
+          top = aerospaceWindowGap;
+          right = aerospaceWindowGap;
+        };
+      };
+      mode = {
+        main.binding = {
+          "${meh}-slash" = "layout tiles horizontal vertical";
+          "${meh}-comma" = "layout accordion vertical horizontal";
+
+          "${meh}-f" = "fullscreen";
+          "${hyper}-f" = "layout floating tiling";
+          "${hyper}-r" = "flatten-workspace-tree";
+
+          "${meh}-h" = "focus left";
+          "${meh}-j" = "focus down";
+          "${meh}-k" = "focus up";
+          "${meh}-l" = "focus right";
+
+          "${hyper}-h" = "move left";
+          "${hyper}-j" = "move down";
+          "${hyper}-k" = "move up";
+          "${hyper}-l" = "move right";
+
+          "${meh}-equal" = "balance-sizes";
+          "${hyper}-minus" = "resize smart -50";
+          "${hyper}-equal" = "resize smart +50";
+
+          ctrl-1 = "workspace 1";
+          ctrl-2 = "workspace 2";
+          ctrl-3 = "workspace 3";
+
+          "${meh}-1" = "move-node-to-workspace 1";
+          "${meh}-2" = "move-node-to-workspace 2";
+          "${meh}-3" = "move-node-to-workspace 3";
+
+          "${hyper}-1" = "move-node-to-monitor 1";
+          "${hyper}-2" = "move-node-to-monitor 2";
+          "${hyper}-3" = "move-node-to-monitor 3";
+
+          "${meh}-tab" = "focus-back-and-forth";
+          "${hyper}-tab" = "workspace-back-and-forth";
+
+          "${meh}-semicolon" = "mode service";
+        };
+        service.binding = {
+          esc = [
+            "reload-config"
+            "mode main"
+          ];
+
+          h = [
+            "join-with left"
+            "mode main"
+          ];
+          j = [
+            "join-with down"
+            "mode main"
+          ];
+          k = [
+            "join-with up"
+            "mode main"
+          ];
+          l = [
+            "join-with right"
+            "mode main"
+          ];
+
+          "${meh}-h" = [
+            "join-with left"
+            "layout v_accordion"
+            "mode main"
+          ];
+          "${meh}-j" = [
+            "join-with down"
+            "layout h_accordion"
+            "mode main"
+          ];
+          "${meh}-k" = [
+            "join-with up"
+            "layout h_accordion"
+            "mode main"
+          ];
+          "${meh}-l" = [
+            "join-with right"
+            "layout v_accordion"
+            "mode main"
+          ];
+        };
+      };
+    };
+  };
+
+  services.jankyborders = {
+    enable = true;
+    active_color = "0xFFA6DA95";
+    width = 8.0;
+    hidpi = true;
+    inactive_color = "";
+  };
 
   system.defaults = {
     NSGlobalDomain = {
@@ -64,7 +222,6 @@
         "/System/Applications/Notes.app"
         "/System/Applications/Messages.app"
         "/System/Applications/Music.app"
-        "/Applications/Firefox.app"
         "/Applications/Ghostty.app"
         "/System/Applications/Passwords.app"
       ];
