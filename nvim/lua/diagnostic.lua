033a06809292dc0b790860eff2664b85688fc159
Jacqueline Askins <askinsjacqueline@gmail.com>
2025-04-03 19:31:40 -0400

    chore(neovim.diagnostic): go back to just current line virtual lines

--- nvim/lua/diagnostic.lua
+++ nvim/lua/diagnostic.lua
@@ -9,27 +9,6 @@ vim.diagnostic.config({
   severity_sort = true,
   update_in_insert = false,
   jump = { float = true },
-  -- Inspired by https://www.reddit.com/r/neovim/comments/1jpbc7s/disable_virtual_text_if_there_is_diagnostic_in/
   virtual_lines = { current_line = true },
-  virtual_text = {
-    current_line = false,
-    source = 'if_many',
-    spacing = 1,
-    format = function(diagnostic)
-      if diagnostic.lnum == vim.fn.line('.') - 1 then
-        return nil
-      end
-
-      return diagnostic.message
-    end,
-  },
-})
-
-require('utils').augroup('diagnostic_cursor_hold', {
-  {
-    'CursorHold',
-    callback = function()
-      vim.diagnostic.show(nil, 0)
-    end,
-  },
+  virtual_text = false,
 })
