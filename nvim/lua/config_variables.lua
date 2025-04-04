93e2cf6aa458670d1be3f71e7b1b39d2d202307e
Jacqueline Askins <askinsjacqueline@gmail.com>
2025-04-04 19:45:25 -0400

    chore(neovim): clean up some types

--- nvim/lua/config_variables.lua
+++ nvim/lua/config_variables.lua
@@ -4,7 +4,7 @@
 
 ---@module 'gx'
 
----@class MyConfig
+---@class (exact) MyConfig
 ---@field is_personal_machine boolean
 ---@field additional_servers table<string, AdditionalServer>
 ---@field supported_servers string[]
@@ -13,8 +13,6 @@
 ---@field supported_linters string[]
 ---@field border_style string
 ---@field custom_gx_handlers GxHandler[]
-
----@class (exact) MyConfig
 MY_CONFIG = {
   is_personal_machine = false,
   additional_servers = {},
