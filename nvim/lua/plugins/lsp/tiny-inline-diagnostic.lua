39eb053f11ae53b6267c47fac4bdfd95108a26a5
Jacqueline Askins <askinsjacqueline@gmail.com>
2025-04-20 13:52:00 -0400

    feat(neovim): add tiny-inline-diagnostic plugin

---
+++ nvim/lua/plugins/lsp/tiny-inline-diagnostic.lua
@@ -0,0 +1,13 @@
+---@type LazySpec
+return {
+  'rachartier/tiny-inline-diagnostic.nvim',
+  event = 'VeryLazy',
+  priority = 1000,
+  opts = {
+    options = {
+      show_all_diags_on_cursorline = true,
+      show_source = { enabled = true, if_many = true },
+    },
+    preset = 'powerline',
+  },
+}
