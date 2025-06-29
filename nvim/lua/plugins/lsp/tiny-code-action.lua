461ca47f9deecc0b03cd54d625dc981d26567d1b
Jacqueline Askins <askinsjacqueline@gmail.com>
2025-06-29 00:59:17 -0400

    feat(neovim): add tiny-code-action plugin

---
+++ nvim/lua/plugins/lsp/tiny-code-action.lua
@@ -0,0 +1,26 @@
+---@type LazySpec
+return {
+  'rachartier/tiny-code-action.nvim',
+  keys = {
+    {
+      'gra',
+      function()
+        require('tiny-code-action').code_action({})
+      end,
+    },
+  },
+  dependencies = { 'nvim-lua/plenary.nvim', 'folke/snacks.nvim' },
+  init = function()
+    -- For some reason this plugin defaults to `DiagnosticWarning` instead of `DiagnosticWarn`
+    vim.api.nvim_set_hl(0, 'DiagnosticWarning', { link = 'DiagnosticWarn' })
+  end,
+  opts = {
+    backend = 'delta',
+    backend_opts = {
+      delta = {
+        args = {}, -- Clearing args so line number highlights are correct
+      },
+    },
+    picker = 'snacks',
+  },
+}
