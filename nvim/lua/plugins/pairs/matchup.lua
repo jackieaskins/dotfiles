850966f46852701eef9a342b664e291feb12579d
Jacqueline Askins <askinsjacqueline@gmail.com>
2025-07-13 19:49:23 -0400

    fix(neovim.matchup): support updated matchup config

--- nvim/lua/plugins/pairs/matchup.lua
+++ nvim/lua/plugins/pairs/matchup.lua
@@ -1,22 +1,16 @@
 ---@type LazySpec
 return {
   'andymass/vim-matchup',
-  dependencies = { 'nvim-treesitter/nvim-treesitter' },
   event = 'VeryLazy',
   init = function()
     vim.g.loaded_matchparen = 1
     vim.g.matchup_matchparen_offscreen = {}
     vim.g.matchup_matchparen_deferred = 1
     vim.g.matchup_matchparen_hi_surround_always = 1
+    vim.g.matchup_treesitter_disable_virtual_text = 1
   end,
-  config = function()
-    ---@diagnostic disable-next-line: missing-fields
-    require('nvim-treesitter.configs').setup({
-      matchup = {
-        enable = true,
-        enable_quotes = true,
-        disable_virtual_text = true,
-      },
-    })
-  end,
+  ---@module 'match-up'
+  ---@type matchup.Config
+  ---@diagnostic disable-next-line: missing-fields
+  opts = {},
 }
