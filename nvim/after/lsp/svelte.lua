f90965c675b5df8652c3968ca3afa276bde243a7
Jacqueline Askins <askinsjacqueline@gmail.com>
2025-07-19 19:05:14 -0400

    chore(neovim.lsp): remove svelte ts/js watcher that is not built-in

--- nvim/after/lsp/svelte.lua
+++ nvim/after/lsp/svelte.lua
@@ -11,17 +11,4 @@ return {
       },
     },
   },
-  on_attach = function(client, bufnr)
-    require('lsp.utils').setup_auto_close_tag(client, bufnr, 'html/tag')
-
-    require('utils').augroup('svelte_tsjschanges', {
-      {
-        { 'BufWritePost' },
-        pattern = { '*.js', '*.ts' },
-        callback = function(args)
-          client:notify('$/onDidChangeTsOrJsFile', { uri = args.match })
-        end,
-      },
-    })
-  end,
 }
