21136ee4adcaed38dd7f8e03194ae5d348016736
Jacqueline Askins <askinsjacqueline@gmail.com>
2025-08-03 22:08:41 -0400

    feat(tmuxinator): define dotfiles tmuxinator config using nix

--- nix/home/modules/tmux.nix
+++ nix/home/modules/tmux.nix
@@ -29,4 +29,20 @@ in
       enable = true;
     };
   };
+
+  home.file."/.config/tmuxinator/dotfiles.yml" = {
+    text = # zsh
+      ''
+        name: dotfiles
+        root: ~
+
+        windows:
+          - dots:
+              root: ~/dotfiles
+          - custom:
+              root: ~/dotfiles_custom
+          - plugins:
+              root: ~/.local/share/nvim/lazy
+      '';
+  };
 }
