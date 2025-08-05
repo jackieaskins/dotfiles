f5864dd3024e0ca76762da6c299c5077c52f74f2
Jacqueline Askins <askinsjacqueline@gmail.com>
2025-08-05 19:31:55 -0400

    chore(hammerspoon): improve window movement with enhanced window hack

--- hammerspoon/twm/supportedLayouts.lua
+++ hammerspoon/twm/supportedLayouts.lua
@@ -8,6 +8,10 @@
 
 local stackGap = CUSTOM.twmStackGap or 15
 
+---@param screenFrame ScreenFrame
+---@param gap number
+---@param numCols number
+---@return number
 local function getWinWidth(screenFrame, gap, numCols)
   -- Total width of the screen
   -- Minus the number of vertical gaps (one less than the number of columns)
@@ -15,11 +19,36 @@ local function getWinWidth(screenFrame, gap, numCols)
   return (screenFrame.w - (gap * (numCols - 1))) / numCols
 end
 
+---@param screenFrame ScreenFrame
+---@param gap number
+---@param numRows number
+---@return number
 local function getWinHeight(screenFrame, gap, numRows)
   -- Total height of the screen
   -- Minus the number of horizontal gaps (one less than the number of rows)
   -- evenly shared between each row
   return (screenFrame.h - (gap * (numRows - 1))) / numRows
+end
+
+---@param window hs.window
+---@param frame hs.geometry
+---Set window frame with workaround for applications with AXEnhancedUserInterface
+local function setWindowFrame(window, frame)
+  -- https://github.com/Hammerspoon/hammerspoon/issues/3224#issuecomment-1294359070
+
+  ---@class hs.axuielement
+  local appElement = hs.axuielement.applicationElement(window:application())
+  local isEnhanced = appElement.AXEnhancedUserInterface
+
+  if isEnhanced then
+    appElement.AXEnhancedUserInterface = false
+  end
+
+  window:setFrame(frame)
+
+  if isEnhanced then
+    appElement.AXEnhancedUserInterface = true
+  end
 end
 
 local M = {}
@@ -43,7 +72,7 @@ function M.stack(windows, screenFrame)
       y = screenFrame.y + (2 * stackGap)
     end
 
-    window:setFrame({
+    setWindowFrame(window, {
       x = screenFrame.x,
       y = y,
       w = screenFrame.w,
@@ -60,14 +89,14 @@ function M.tall(windows, screenFrame, gap)
 
   for index, window in ipairs(windows) do
     if index == 1 then
-      window:setFrame({
+      setWindowFrame(window, {
         x = screenFrame.x,
         y = screenFrame.y,
         w = winWidth,
         h = screenFrame.h,
       })
     else
-      window:setFrame({
+      setWindowFrame(window, {
         x = screenFrame.x + winWidth + gap,
         y = screenFrame.y + ((index - 2) * (winHeight + gap)),
         w = winWidth,
@@ -85,14 +114,14 @@ function M.rtall(windows, screenFrame, gap)
 
   for index, window in ipairs(windows) do
     if index == 1 then
-      window:setFrame({
+      setWindowFrame(window, {
         x = screenFrame.x + winWidth + gap,
         y = screenFrame.y,
         w = winWidth,
         h = screenFrame.h,
       })
     else
-      window:setFrame({
+      setWindowFrame(window, {
         x = screenFrame.x,
         y = screenFrame.y + ((index - 2) * (winHeight + gap)),
         h = winHeight,
@@ -119,7 +148,7 @@ function M.grid(windows, screenFrame, gap)
     local x = screenFrame.x + math.floor((index - 1) / numRows) * (winWidth + gap)
     local y = screenFrame.y + ((index - 1) % numRows) * (winHeight + gap)
 
-    window:setFrame({
+    setWindowFrame(window, {
       x = x,
       y = y,
       -- Make last window fill remaining height
@@ -135,7 +164,7 @@ function M.columns(windows, screenFrame, gap)
   local winWidth = getWinWidth(screenFrame, gap, #windows)
 
   for index, window in ipairs(windows) do
-    window:setFrame({
+    setWindowFrame(window, {
       x = screenFrame.x + (index - 1) * (winWidth + gap),
       y = screenFrame.y,
       w = winWidth,
@@ -150,7 +179,7 @@ function M.rows(windows, screenFrame, gap)
   local winHeight = getWinHeight(screenFrame, gap, #windows)
 
   for index, window in ipairs(windows) do
-    window:setFrame({
+    setWindowFrame(window, {
       x = screenFrame.x,
       y = screenFrame.y + (index - 1) * (winHeight + gap),
       w = screenFrame.w,
