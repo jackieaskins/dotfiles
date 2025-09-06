b3177e2df5689e3d6e595eedaa272f810c0d9706
Jacqueline Askins <askinsjacqueline@gmail.com>
2025-09-06 16:25:44 -0400

    feat(nix): add extras and tmux syntax to bat

--- nix/home/modules/bat.nix
+++ nix/home/modules/bat.nix
@@ -1,4 +1,22 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  programs.bat.enable = true;
+  programs.bat = {
+    enable = true;
+    extraPackages = [
+      pkgs.bat-extras.batdiff
+      pkgs.bat-extras.batgrep
+      pkgs.bat-extras.batman
+    ];
+    syntaxes = {
+      tmux = {
+        src = pkgs.fetchFromGitHub {
+          owner = "gerardroche";
+          repo = "sublime-tmux";
+          rev = "master";
+          hash = "sha256-c7WJOmrYi8MLCU19O8KGNfV7YxSO+SdVmxtwsdkIxtQ=";
+        };
+        file = "Tmux.sublime-syntax";
+      };
+    };
+  };
 }
