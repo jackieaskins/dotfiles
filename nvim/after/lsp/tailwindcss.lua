591d9385e79dda974c563e5bff8a3e5e5a41f093
Jacqueline Askins <askinsjacqueline@gmail.com>
2025-10-12 20:17:57 -0400

    fix(neovim.lsp): fix tailwindcss root dir

--- nvim/after/lsp/tailwindcss.lua
+++ nvim/after/lsp/tailwindcss.lua
@@ -1,7 +1,15 @@
+local tailwind_path = 'node_modules/tailwindcss'
+
 ---@type vim.lsp.Config
 return {
   root_dir = function(bufnr, on_dir)
     local fname = vim.api.nvim_buf_get_name(bufnr)
-    on_dir(vim.fs.dirname(vim.fs.find({ 'node_modules/tailwindcss' }, { path = fname, upward = true })[1]))
+
+    local dir = vim.fs.find({ tailwind_path }, {
+      path = fname,
+      upward = true,
+    })[1]
+
+    on_dir(vim.fs.dirname(dir and dir:gsub(tailwind_path, '')))
   end,
 }
