44561a6681eb254ef0fe02c97cdd839b4aa97f76
Jacqueline Askins <askinsjacqueline@gmail.com>
2025-11-02 21:08:08 -0500

    chore(neovim): remove luvit-meta plugin now that vim.uv types are built-in

--- nvim/lua/plugins/lsp/lazydev.lua
+++ nvim/lua/plugins/lsp/lazydev.lua
@@ -1,36 +1,32 @@
 ---@type LazySpec
 return {
-  { 'Bilal2453/luvit-meta', lazy = true },
+  'folke/lazydev.nvim',
+  ft = 'lua',
   ---@module 'lazydev'
   ---@type lazydev.Config
-  {
-    'folke/lazydev.nvim',
-    ft = 'lua',
-    opts = {
-      enabled = function(root_dir)
-        if vim.g.lazydev_enabled ~= nil then
-          return vim.g.lazydev_enabled
-        end
+  opts = {
+    enabled = function(root_dir)
+      if vim.g.lazydev_enabled ~= nil then
+        return vim.g.lazydev_enabled
+      end
 
-        return not vim.tbl_contains(
-          vim.tbl_map(function(path)
-            return vim.fn.expand(path)
-          end, {
-            '~/dotfiles/hammerspoon',
-            '~/dotfiles/wezterm',
-          }),
-          root_dir
-        )
-      end,
-      library = {
-        'lazy.nvim',
-        'luvit-meta/library/uv.lua',
-        'catppuccin',
-        'nvim-lspconfig',
-        'mini.icons',
-        'mini.files',
-        'snacks.nvim',
-      },
+      return not vim.tbl_contains(
+        vim.tbl_map(function(path)
+          return vim.fn.expand(path)
+        end, {
+          '~/dotfiles/hammerspoon',
+          '~/dotfiles/wezterm',
+        }),
+        root_dir
+      )
+    end,
+    library = {
+      'lazy.nvim',
+      'catppuccin',
+      'nvim-lspconfig',
+      'mini.icons',
+      'mini.files',
+      'snacks.nvim',
     },
   },
 }
