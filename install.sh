f83ab91c9c006bc7c354da509d931895bd4f3bfb
Jacqueline Askins <askinsjacqueline@gmail.com>
2026-01-29 10:22:53 -0500

    fix(install): improve install script, install nix-darwin first

--- install.sh
+++ install.sh
@@ -1,33 +1,60 @@
 #!/bin/bash
 
-if [[ -f $HOME/.config/nix/nix.conf ]]; then
-  echo "$HOME/.config/nix/nix.conf already exists. Not editing."
-else
-  echo "Creating $HOME/.config/nix/nix.conf."
-  mkdir -p $HOME/.config/nix
-  echo "extra-experimental-features = nix-command flakes" >> $HOME/.config/nix/nix.conf
-fi
+#--------------------------------------------------------------------#
+#                             Nix Config                             #
+#--------------------------------------------------------------------#
+configure_nix() {
+  local nix_config_dir=$HOME/.config/nix
+  local nix_config_file=$nix_config_dir/nix.conf
 
-if [[ -d $HOME/.config/home-manager ]]; then
-  echo "$HOME/.config/home-manager already exists. Not creating symlink."
-else
-  echo "Creating $HOME/.config/home-manager symlink."
-  ln -s ~/dotfiles/nix $HOME/.config/home-manager
-fi
-
-echo "Activating home-manager..."
-echo ""
-nix run home-manager -- switch -b backup
-
-if [[ $OSTYPE == 'darwin'* ]]; then
-  if [[ -d /etc/nix-darwin ]]; then
-    echo "/etc/nix-darwin already exists. Not creating symlink."
+  if [[ -f "$nix_config_file" ]]; then
+    echo "$nix_config_file already exists. Not editing."
   else
-    echo "Creating /etc/nix-darwin symlink."
-    sudo ln -s ~/dotfiles/nix /etc/nix-darwin
+    echo "Creating $nix_config_file..."
+    mkdir -p "$nix_config_dir"
+    echo "extra-experimental-features = nix-command flakes" >> "$nix_config_file"
+  fi
+}
+
+#--------------------------------------------------------------------#
+#                       Nix Darwin Activation                        #
+#--------------------------------------------------------------------#
+activate_nix_darwin() {
+  if [[ $OSTYPE == 'darwin'* ]]; then
+    local nix_darwin_dir=/etc/nix-darwin
+
+    if [[ -d "$nix_darwin_dir" ]]; then
+      echo "$nix_darwin_dir already exists. Not creating symlink."
+    else
+      echo "Creating $nix_darwin_dir symlink..."
+      sudo ln -s ~/dotfiles/nix /etc/nix-darwin
+    fi
+
+    echo "Activating nix-darwin..."
+    sudo nix run nix-darwin --extra-experimental-features 'nix-command flakes' -- switch || exit 1
+  fi
+}
+
+#--------------------------------------------------------------------#
+#                      Home Manager Activation                       #
+#--------------------------------------------------------------------#
+activate_home_manager() {
+  local home_manager_dir=$HOME/.config/home-manager
+
+  if [[ -d "$home_manager_dir" ]]; then
+    echo "$home_manager_dir already exists. Not creating symlink."
+  else
+    echo "Creating $home_manager_dir symlink..."
+    ln -s ~/dotfiles/nix "$home_manager_dir"
   fi
 
-  echo "Activating nix-darwin..."
-  echo ""
-  sudo nix run nix-darwin -- switch
-fi
+  echo "Activating home-manager..."
+  nix run home-manager -- switch -b backup || exit 1
+}
+
+#--------------------------------------------------------------------#
+#                               Script                               #
+#--------------------------------------------------------------------#
+configure_nix
+activate_nix_darwin
+activate_home_manager
