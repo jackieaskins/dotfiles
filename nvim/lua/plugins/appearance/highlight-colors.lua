8113c47ae259268f335cd57dee50c5d5b3753fab
Jacqueline Askins <askinsjacqueline@gmail.com>
2026-01-31 14:41:05 -0500

    feat(neovim.highlight-colors): highlight ctp css vars

--- nvim/lua/plugins/appearance/highlight-colors.lua
+++ nvim/lua/plugins/appearance/highlight-colors.lua
@@ -2,10 +2,15 @@
 return {
   'brenoprata10/nvim-highlight-colors',
   opts = function()
+    local custom_colors = {}
+
+    for name, color in pairs(require('colors').get_colors()) do
+      table.insert(custom_colors, { label = 'colors.' .. name, color = color })
+      table.insert(custom_colors, { label = '%-%-ctp%-' .. name, color = color })
+    end
+
     return {
-      custom_colors = require('utils').map_table_to_list(function(color, name)
-        return { label = 'colors.' .. name, color = color }
-      end, require('colors').get_colors()),
+      custom_colors = custom_colors,
       enable_named_colors = false,
       excluded_filetypes = { 'lazy' },
     }
