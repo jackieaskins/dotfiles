f48efb046237487ada52e17add62f8f6e934ce37
Jacqueline Askins <askinsjacqueline@gmail.com>
2026-05-02 22:38:36 -0400

    chore(nix): update flake

--- nix/home/modules/direnv.nix
+++ nix/home/modules/direnv.nix
@@ -1,8 +1,5 @@
 { ... }:
 {
-  # Another update another broken package :)
-  # https://github.com/NixOS/nixpkgs/issues/507531
-  # https://github.com/NixOS/nixpkgs/issues/513019
   nixpkgs.overlays = [
     (_final: _super: {
       direnv = _super.direnv.overrideAttrs (_: {
