4a1c91cb04fe0dfe97e2e896b71edb7f7bbdaadb
Jacqueline Askins <askinsjacqueline@gmail.com>
2026-09-08 11:52:36 -0400

    chore: update flake, add css-vars lsp, clean up lsp configs

--- nix/home/modules/direnv.nix
+++ nix/home/modules/direnv.nix
@@ -1,12 +1,5 @@
 { ... }:
 {
-  nixpkgs.overlays = [
-    (_final: _super: {
-      direnv = _super.direnv.overrideAttrs (_: {
-        doCheck = false;
-      });
-    })
-  ];
   programs.direnv = {
     enable = true;
     enableZshIntegration = true;
---
+++ nvim/after/lsp/css_variables.lua
@@ -0,0 +1,11 @@
+---@type vim.lsp.Config
+return {
+  filetypes = {
+    'svelte',
+    'css',
+    'html',
+    'javascript',
+    'javascriptreact',
+    'typescriptreact',
+  },
+}
